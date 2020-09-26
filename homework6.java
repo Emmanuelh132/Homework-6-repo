de37cc160a6411956f9eea0558dc321214e9a9d3
Emmanuel Hosein <ehosein@njcu.edu>
2020-09-26 19:01:16 +0000

    Changed output on program for the second commit

--- homework6.java
+++ homework6.java
@@ -1,5 +1,5 @@
 public class homework6 {
 	public static void main(String[] args){
-		System.out.println("My name is Emmanuel Hosein");
+		System.out.println("My favorite programming language is java");
 	}	
-}	
+}		
