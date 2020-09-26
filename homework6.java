86402d70467a41d1c4bcd46e4bfa6005b294d414
Emmanuel Hosein <ehosein@njcu.edu>
2020-09-26 19:05:41 +0000

    third commit changed output to why im taking CS407

--- homework6.java
+++ homework6.java
@@ -1,5 +1,5 @@
 public class homework6 {
 	public static void main(String[] args){
-		System.out.println("My favorite programming language is java");
+		System.out.println("I am taking CS407 because I want to learn");
 	}	
 }		
